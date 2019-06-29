8add65bf61c0ec28000d5806a23729b86ea17d9e
Radoslaw Domian <palekelf@o2.pl>
2019-06-29 16:53:08 +0200

    counter 1

---
+++ WindowsFormsApp1/Counter.BusinessLogic/Counter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace Counter.BusinessLogic
+{
+
+	public class CounterDelay
+	{
+
+		public void Count(int countTill, int delayTime, Action<int> whenWaitingIsFinished)
+		{
+			for (int i = 0; i < countTill; i++)
+			{
+				Thread.Sleep(delayTime);
+				whenWaitingIsFinished(i);
+			}
+		}
+	}
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Counter.BusinessLogic;
+
+namespace Counter.WinForms
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		
+		private void StartButton_Click(object sender, EventArgs e)
+		{
+			
+			OutputTextBox.Clear();
+
+			int delayTime = Decimal.ToInt32(InputDelayTime.Value);
+			int countTill = Decimal.ToInt32(CountTill.Value);
+
+			CounterDelay counter = new CounterDelay();
+
+			Action<int> callback = output;
+			//counter.Count(countTill, delayTime, (x)=> OutputTextBox.AppendText($"{x}"));
+
+			counter.Count(countTill, delayTime, callback);
+
+		}
+
+		private void output(int x)
+		{
+			OutputTextBox.AppendText($"{x}");
+		}
+	}
+}
